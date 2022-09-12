138d52d39edc0353903633e117d22d0375af910c
Marat Minchik <devminrat@gmail.com>
2022-09-12 18:14:28 +0300

    added catalog page (category)

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //= ../../../node_modules/@splidejs/splide/dist/js/splide.js
   //= ../../../node_modules/parallax-js/dist/parallax.js
-  //= components/
 
   const header = document.querySelector(".header");
   let scrollPrev = 0;
@@ -17,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
       header.classList.remove("out");
     }
 
-    if (scrolled <= 20) {
+    if (scrolled <= 50) {
       header.classList.add("top");
     } else {
       header.classList.remove("top");
@@ -26,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
     scrollPrev = scrolled;
   });
 
-  if (document.documentElement.scrollTop <= 20) {
+  if (document.documentElement.scrollTop <= 50) {
     header.classList.add("top");
   }
 
@@ -35,186 +34,197 @@ document.addEventListener("DOMContentLoaded", () => {
   const introSliderEl = document.querySelector(".intro-slider");
   const introBg = document.querySelector(".intro-bg");
 
-  const introSlider = new Splide(".splide", {
-    type: "fade",
-    rewind: true,
-    autoplay: true,
-    arrows: false,
-    resetProgress: !1,
-    interval: 5000,
-    pagination: false,
-    autoHeight: !0,
-    pauseOnHover: true,
-  }).mount();
-
   // parallax intro
 
-  const introItem = document.querySelectorAll(".intro-item");
+  if (introSliderEl) {
+    const introSlider = new Splide(".splide", {
+      type: "fade",
+      rewind: true,
+      autoplay: true,
+      arrows: false,
+      resetProgress: !1,
+      interval: 5000,
+      pagination: false,
+      autoHeight: !0,
+      pauseOnHover: true,
+    }).mount();
 
-  introItem.forEach((el) => {
-    new Parallax(el, {
-      selector: ".layer",
+    const introItem = document.querySelectorAll(".intro-item");
+
+    introItem.forEach((el) => {
+      new Parallax(el, {
+        selector: ".layer",
+      });
     });
-  });
 
-  const iSliderNavItems = document.querySelectorAll(".is-nav__item");
-  const inc = document.querySelectorAll(".icon-nav-circle");
+    const iSliderNavItems = document.querySelectorAll(".is-nav__item");
+    const inc = document.querySelectorAll(".icon-nav-circle");
 
-  function setIntroBg(idx) {
-    switch (idx) {
-      case 0:
-        introBg.classList.add("ibg-chups");
-        introBg.classList.remove("ibg-cola", "ibg-fanta", "ibg-pepper");
-        break;
-      case 1:
-        introBg.classList.add("ibg-cola");
-        introBg.classList.remove("ibg-chups", "ibg-fanta", "ibg-pepper");
-        break;
-      case 2:
-        introBg.classList.add("ibg-fanta");
-        introBg.classList.remove("ibg-cola", "ibg-chups", "ibg-pepper");
-        break;
-      case 3:
-        introBg.classList.add("ibg-pepper");
-        introBg.classList.remove("ibg-cola", "ibg-fanta", "ibg-chups");
-        break;
-      default:
-        break;
-    }
-  }
-
-  function setActiveNavItem(idx) {
-    iSliderNavItems.forEach((el) => {
-      el.classList.remove("active");
-    });
-    iSliderNavItems[idx].classList.add("active");
-  }
-
-  function animateNavCircleSvg(idx) {
-    let currentAnimate = inc[idx].querySelector("animate");
-    currentAnimate.beginElement();
-  }
-
-  iSliderNavItems.forEach((el, idx) => {
-    el.addEventListener("click", (e) => {
-      setIntroBg(idx);
-      introSlider.go(idx);
-      if (!el.classList.contains("active")) {
-        animateNavCircleSvg(idx);
+    function setIntroBg(idx) {
+      switch (idx) {
+        case 0:
+          introBg.classList.add("ibg-chups");
+          introBg.classList.remove("ibg-cola", "ibg-fanta", "ibg-pepper");
+          break;
+        case 1:
+          introBg.classList.add("ibg-cola");
+          introBg.classList.remove("ibg-chups", "ibg-fanta", "ibg-pepper");
+          break;
+        case 2:
+          introBg.classList.add("ibg-fanta");
+          introBg.classList.remove("ibg-cola", "ibg-chups", "ibg-pepper");
+          break;
+        case 3:
+          introBg.classList.add("ibg-pepper");
+          introBg.classList.remove("ibg-cola", "ibg-fanta", "ibg-chups");
+          break;
+        default:
+          break;
       }
-    });
-  });
+    }
 
-  function onInitIntroSlider(idx) {
-    setIntroBg(idx);
-    setActiveNavItem(idx);
-    animateNavCircleSvg(idx);
+    function setActiveNavItem(idx) {
+      iSliderNavItems.forEach((el) => {
+        el.classList.remove("active");
+      });
+      iSliderNavItems[idx].classList.add("active");
+    }
+
+    function animateNavCircleSvg(idx) {
+      let currentAnimate = inc[idx].querySelector("animate");
+      currentAnimate.beginElement();
+    }
+
+    iSliderNavItems.forEach((el, idx) => {
+      el.addEventListener("click", (e) => {
+        setIntroBg(idx);
+        introSlider.go(idx);
+        if (!el.classList.contains("active")) {
+          animateNavCircleSvg(idx);
+        }
+      });
+    });
+
+    function onInitIntroSlider(idx) {
+      setIntroBg(idx);
+      setActiveNavItem(idx);
+      animateNavCircleSvg(idx);
+    }
+
+    onInitIntroSlider(0);
+
+    introSlider.on("moved", function (idx) {
+      animateNavCircleSvg(idx);
+      setActiveNavItem(idx);
+      setIntroBg(idx);
+    });
+
+    introSliderEl.addEventListener("mouseenter", () => {
+      inc.forEach((el) => {
+        el.pauseAnimations();
+      });
+    });
+    introSliderEl.addEventListener("mouseleave", () => {
+      inc.forEach((el) => {
+        el.unpauseAnimations();
+      });
+    });
   }
-
-  onInitIntroSlider(0);
-
-  introSlider.on("moved", function (idx) {
-    animateNavCircleSvg(idx);
-    setActiveNavItem(idx);
-    setIntroBg(idx);
-  });
-
-  introSliderEl.addEventListener("mouseenter", () => {
-    inc.forEach((el) => {
-      el.pauseAnimations();
-    });
-  });
-  introSliderEl.addEventListener("mouseleave", () => {
-    inc.forEach((el) => {
-      el.unpauseAnimations();
-    });
-  });
 
   // popular svg animation
 
   const pib = document.querySelector("#pop-item-bg");
+  const popularItems = document.querySelectorAll(".mproduct-item");
 
-  function animatePopBubbleSvg() {
-    let animatedEl = pib.querySelectorAll("animate");
-    let animatedElTr = pib.querySelector("animateTransform");
-    animatedElTr.beginElement();
-    for (let i = 0; i < animatedEl.length; i++) {
-      animatedEl[i].beginElement();
+  if (popularItems.length > 0) {
+    function animatePopBubbleSvg() {
+      let animatedEl = pib.querySelectorAll("animate");
+      let animatedElTr = pib.querySelector("animateTransform");
+      animatedElTr.beginElement();
+      for (let i = 0; i < animatedEl.length; i++) {
+        animatedEl[i].beginElement();
+      }
     }
-  }
 
-  const popularItems = document.querySelectorAll(".popular-item");
-
-  popularItems.forEach((el) => {
-    el.addEventListener("mouseenter", (e) => {
-      animatePopBubbleSvg();
+    popularItems.forEach((el) => {
+      el.addEventListener("mouseenter", (e) => {
+        animatePopBubbleSvg();
+      });
     });
-  });
+  }
 
   // popular parallax bg
 
-  let startPosPopBg = -300;
+  let popularMain = document.querySelector(".popular");
 
-  ScrollTrigger.create({
-    trigger: ".popular",
-    start: "top bottom",
-    endTrigger: ".weworks",
-    end: "top center",
-    onUpdate: (self) => {
-      let factor = 300;
-      console.log(factor * self.progress.toFixed(2));
-      self.trigger.style.backgroundPosition = `right top ${
-        startPosPopBg + factor * self.progress.toFixed(2)
-      }px`;
-    },
-  });
+  if (popularMain) {
+    let startPosPopBg = -300;
+
+    ScrollTrigger.create({
+      trigger: ".popular",
+      start: "top bottom",
+      endTrigger: ".weworks",
+      end: "top center",
+      onUpdate: (self) => {
+        let factor = 300;
+        self.trigger.style.backgroundPosition = `right top ${
+          startPosPopBg + factor * self.progress.toFixed(2)
+        }px`;
+      },
+    });
+  }
 
   // parallax benefits
 
   const benefitsContent = document.querySelector(".benefits-content");
 
-  const benefitsParallax = new Parallax(benefitsContent);
-
   // clients animation
 
-  const clientsTopFT = gsap.fromTo(
-    ".clients-top",
-    {
-      x: -1340,
-    },
-    {
-      x: 0,
-      ease: "none",
-    }
-  );
-  const clientsBottomFT = gsap.fromTo(
-    ".clients-bottom",
-    {
-      x: 0,
-    },
-    {
-      x: -1340,
-      ease: "none",
-    }
-  );
-  let clientsTopST = ScrollTrigger.create({
-    trigger: ".clients",
-    start: "200 bottom",
-    end: "bottom top",
-    scrub: 1.5,
-    animation: clientsTopFT,
-  });
-  let clientsBottomST2 = ScrollTrigger.create({
-    trigger: ".clients",
-    start: "200 bottom",
-    end: "bottom top",
-    scrub: 1.5,
-    animation: clientsBottomFT,
-  });
+  if (benefitsContent) {
+    const benefitsParallax = new Parallax(benefitsContent);
+
+    const clientsTopFT = gsap.fromTo(
+      ".clients-top",
+      {
+        x: -1340,
+      },
+      {
+        x: 0,
+        ease: "none",
+      }
+    );
+    const clientsBottomFT = gsap.fromTo(
+      ".clients-bottom",
+      {
+        x: 0,
+      },
+      {
+        x: -1340,
+        ease: "none",
+      }
+    );
+    let clientsTopST = ScrollTrigger.create({
+      trigger: ".clients",
+      start: "200 bottom",
+      end: "bottom top",
+      scrub: 1.5,
+      animation: clientsTopFT,
+    });
+    let clientsBottomST2 = ScrollTrigger.create({
+      trigger: ".clients",
+      start: "200 bottom",
+      end: "bottom top",
+      scrub: 1.5,
+      animation: clientsBottomFT,
+    });
+  }
 
   // animate fruits and berries
 
   const animFruits = document.querySelectorAll(".anim-fruit");
 
-  
+  if (animFruits.length > 0) {
+  }
+
+  //= components/catalog-filter.js
 });
---
+++ src/assets/js/components/catalog-filter.js
@@ -0,0 +1,73 @@
+// catalog page filtering
+
+const cpFilterTriggers = document.querySelectorAll(
+  ".cp-content__filter-trigger"
+);
+
+if (cpFilterTriggers.length > 0) {
+  const filterList = document.querySelector(".filter-list__content");
+  const fiterCheckboxes = document.querySelectorAll(
+    ".cp-filter__modal-item .custom-checkbox"
+  );
+  const filterReset = document.querySelector(".cp-content__filter-reset");
+
+  cpFilterTriggers.forEach((el) => {
+    el.addEventListener("click", () => {
+      el.classList.toggle("active");
+      el.nextElementSibling.classList.toggle("active");
+    });
+  });
+
+  function showFilterReset() {
+    if (document.querySelectorAll(".filter-list__item").length === 0) {
+      filterReset.style.display = "none";
+    } else {
+      filterReset.style.display = "";
+    }
+  }
+  showFilterReset();
+
+  function createFilterItem(name) {
+    let filterItem = document.createElement("div");
+    filterItem.classList.add("filter-list__item");
+    filterItem.dataset.name = name;
+    filterItem.innerHTML = ` <p class="filter-list__item-name">${name}</p>
+    <svg class="filter-list__item-close">
+        <use href="#cross"></use>
+    </svg>`;
+
+    filterItem.addEventListener("click", (e) => {
+      let target = e.target;
+
+      if (target.classList.contains("filter-list__item-close")) {
+        filterItem.remove();
+        document.querySelector(`#${name}`).checked = false;
+      }
+    });
+
+    filterList.append(filterItem);
+
+    showFilterReset();
+  }
+
+  function removeFilterItem(name) {
+    let item = document.querySelector(`[data-name='${name}']`);
+
+    item.remove();
+    showFilterReset();
+  }
+
+  fiterCheckboxes.forEach((el) => {
+    el.addEventListener("change", () => {
+      if (el.checked) {
+        createFilterItem(el.value);
+      } else {
+        removeFilterItem(el.value);
+      }
+    });
+  });
+
+  filterReset.addEventListener("click", () => {
+    filterList.innerHTML = "";
+  });
+}
